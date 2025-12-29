5578a7abec5ca85dcbc8d4d1836beef9c8214942
TruongNoSimp <truongdeptryqua9@gmail.com>
2025-12-30 02:26:36 +0700

    fix ngc ten

--- src/main/java/org/example/DAO/RewardDAO.java
+++ src/main/java/org/example/DAO/RewardDAO.java
@@ -9,8 +9,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-public class RewardDAO {
 
+public class RewardDAO {
     public List<Reward> findAll() {
         List<Reward> list = new ArrayList<>();
         String sql = """
@@ -20,17 +20,22 @@ public class RewardDAO {
             FROM reward r
             JOIN student s ON r.student_id = s.student_id
         """;
+
         try (Connection c = dbConn.getConnection();
              PreparedStatement ps = c.prepareStatement(sql);
              ResultSet rs = ps.executeQuery()) {
+
             while (rs.next()) {
                 Reward r = new Reward();
                 r.setRewardId(rs.getInt("reward_id"));
                 r.setStudentId(rs.getString("student_id"));
+
+
                 r.setStudentName(
-                        rs.getString("student_firstName") + " " +
-                                rs.getString("student_lastName")
+                        rs.getString("student_lastName") + " " +
+                                rs.getString("student_firstName")
                 );
+
                 r.setRewardDate(rs.getDate("reward_date"));
                 r.setRewardNote(rs.getString("reward_note"));
                 r.setRewardQuyetDinh(rs.getString("reward_quyetdinh"));
@@ -41,6 +46,8 @@ public class RewardDAO {
         }
         return list;
     }
+
+
     public List<String> getStudentIds() {
         List<String> list = new ArrayList<>();
         String sql = "SELECT student_id FROM student";
@@ -57,6 +64,8 @@ public class RewardDAO {
         }
         return list;
     }
+
+
     public String getStudentNameById(String studentId) {
         String sql = "SELECT student_firstName, student_lastName FROM student WHERE student_id = ?";
 
@@ -66,7 +75,9 @@ public class RewardDAO {
             ps.setString(1, studentId);
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
-                    return rs.getString("student_firstName") + " " + rs.getString("student_lastName");
+
+                    return rs.getString("student_lastName") + " " +
+                            rs.getString("student_firstName");
                 }
             }
         } catch (SQLException e) {
@@ -74,6 +85,8 @@ public class RewardDAO {
         }
         return "";
     }
+
+
     public void insert(Reward r) throws SQLException {
         Connection conn = dbConn.getConnection();
         String sql = "INSERT INTO reward (student_id, reward_date, reward_note, reward_quyetdinh) VALUES (?, ?, ?, ?)";
@@ -88,6 +101,8 @@ public class RewardDAO {
         ps.close();
         conn.close();
     }
+
+
     public int update(Reward r) throws SQLException {
         Connection conn = dbConn.getConnection();
         String sql = "UPDATE reward SET student_id = ?, reward_date = ?, reward_note = ?, reward_quyetdinh = ? WHERE reward_id = ?";
@@ -98,17 +113,21 @@ public class RewardDAO {
         ps.setString(3, r.getRewardNote());
         ps.setString(4, r.getRewardQuyetDinh());
         ps.setInt(5, r.getRewardId());
+
         int row = ps.executeUpdate();
         ps.close();
         conn.close();
         return row;
     }
+
+
     public int delete(Reward r) throws SQLException {
         Connection conn = dbConn.getConnection();
         String sql = "DELETE FROM reward WHERE reward_id = ?";
 
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setInt(1, r.getRewardId());
+
         int row = ps.executeUpdate();
         ps.close();
         conn.close();
